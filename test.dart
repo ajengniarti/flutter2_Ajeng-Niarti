c1321721959ef6bcb265a450709032b6604f8f69
ajengniarti <125692283+ajengniarti@users.noreply.github.com>
2023-02-28 17:23:16 +0700

    nambah code lagi

--- test.dart
+++ test.dart
@@ -1,3 +1,28 @@
+// double hitungLuasPersegi(double sisi) {
+//   return sisi * sisi;
+// }
+
+// double hitungKelilingPersegi(double sisi) {
+//   return 4 * sisi;
+// }
+
+
+// double hitungKelilingPersegi(double sisi) {
+//   return 4 * sisi;
+
+
+double hitungLuasPersegi(double sisi) {
+  return sisi * sisi;
+}
+
+double hitungKelilingPersegi(double sisi) {
+  return 4 * sisi;
+}
+
+
+double hitungKelilingPersegi(double sisi) {
+  return 4 * sisi;
+
 double hitungLuasPersegi(double sisi) {
   return sisi * sisi;
 }
